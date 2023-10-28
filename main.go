ad186ba4ae6245ec53334e8ef6e2ea89abb5246e
Neal-C <pop6d531@duck.com>
2023-10-28 22:02:44 +0200

    chore: setup database connection

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +22,14 @@ func main() {
 			log.Fatal("hire me! 😮", err)
 		}
 	}
+
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %+v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
