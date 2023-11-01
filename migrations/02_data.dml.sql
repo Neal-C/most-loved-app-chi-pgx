30070642628a27a9b2cdd129a17586d20ce208d6
Neal-C <pop6d531@duck.com>
2023-11-01 21:17:33 +0100

    feat(MIGRATIONS): added data.dml.sql

---
+++ migrations/02_data.dml.sql
@@ -0,0 +1,4 @@
+-- Add migration script here
+INSERT INTO quote (book, quote) VALUES ("Games of Thrones", "fear cuts deeper than swords");
+INSERT INTO quote (book, quote) VALUES ("Hunger Games", "May the odds be with you");
+INSERT INTO quote (book, quote) VALUES ("The 48 laws of power", "rule #1 will shock you");
