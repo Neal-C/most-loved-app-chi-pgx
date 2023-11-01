0aa8b49826498e4563370281ec5295af22558429
Neal-C <pop6d531@duck.com>
2023-11-01 21:21:44 +0100

    fix(MIGRATIONS): forgot default in timestamptz

--- migrations/01_schema.ddl.sql
+++ migrations/01_schema.ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS quote (
     id UUID PRIMARY KEY,
     book VARCHAR(63) NOT NULL,
     quote VARCHAR(255) NOT NULL,
-    inserted_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL,
+    inserted_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     UNIQUE(book, quote)
 )
