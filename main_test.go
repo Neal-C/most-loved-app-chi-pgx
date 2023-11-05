dc17aaff943f4660fb375fae45ecfa0071202a61
Neal-C <pop6d531@duck.com>
2023-11-05 11:07:44 +0100

    feat(MAIN_TEST): moved over configuration and prepared for testing

--- main_test.go
+++ main_test.go
@@ -30,3 +30,57 @@ func checkResponseCode(t *testing.T, expected, actual int) {
         t.Errorf("Expected response code %d. Got %d\n", expected, actual)
     }
 }
+
+
+// pgconfig is a struct that holds the configuration for connecting to a postgres database.
+// each field corresponds to a component of the connection string.
+// the following required environment variables are used to populate the struct:
+//
+//	PG_USER
+//	 PG_PASSWORD
+//	 PG_HOST
+//	 PG_PORT
+//	 PG_DATABASE
+//
+// additionally, the following optional environment variable is used to populate the sslmode:
+//
+//	PG_SSLMODE: must be one of  "", "disable", "allow", "require", "verify-ca", or "verify-full"
+type pgconfig struct {
+	user, database, host, password, port string // required
+	sslMode                              string // optional
+}
+
+func pgConfigFromEnv() (pgconfig, error) {
+	var missing []string
+	// small closures like this can help reduce code duplication and make intent clearer.
+	// you generally pay a small performance penalty for this, but for configuration, it's not a big deal;
+	// you can spare the nanoseconds.
+	// i prefer little helper functions like this to a complicated configuration framework like viper, cobra, envconfig, etc.
+	get := func(key string) string {
+		val := os.Getenv(key)
+		if val == "" {
+			missing = append(missing, key)
+		}
+		return val
+	}
+	cfg := pgconfig{
+		user:     get("POSTGRES_USER"),
+		database: get("POSTGRES_DB"),
+		host:     get("POSTGRES_HOST"),
+		password: get("POSTGRES_PASSWORD"),
+		port:     get("POSTGRES_PORT"),
+		sslMode:  os.Getenv("POSTGRES_SSLMODE"), // optional, so we don't add it to missing
+	}
+	switch cfg.sslMode {
+	case "", "disable", "allow", "require", "verify-ca", "verify-full":
+		// valid sslmode
+	default:
+		return cfg, fmt.Errorf(`invalid sslmode "%s": expected one of "", "disable", "allow", "require", "verify-ca", or "verify-full"`, cfg.sslMode)
+	}
+
+	if len(missing) > 0 {
+		sort.Strings(missing) // sort for consistency in error message
+		return cfg, fmt.Errorf("missing required environment variables: %v", missing)
+	}
+	return cfg, nil
+}
