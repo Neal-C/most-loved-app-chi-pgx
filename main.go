8a03019bd98c2c3155c97f58a9434c0d9b2cf490
Neal-C <pop6d531@duck.com>
2023-11-05 11:07:10 +0100

    feat(MAIN): refactored into functions to make it testable

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"sort"
 	"syscall"
 	"time"
 
@@ -24,6 +23,53 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
+
+type Server struct {
+	Router *chi.Mux
+	Addr   string
+}
+
+func createNewServer(port string) *Server {
+	server := &Server{
+		Addr:   "0.0.0.0:" + port,
+		Router: chi.NewRouter(),
+	}
+
+	return server
+}
+
+func (server *Server) MountHandlers(connectionPool *pgxpool.Pool) {
+	server.Router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{`https:\/\/*`, `http:\/\/*`},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	// Enable httprate request limiter of 100 requests per minute.
+	//
+	// In the code example below, rate-limiting is bound to the request IP address
+	// via the LimitByIP middleware handler.
+	//
+	// To have a single rate-limiter for all requests, use httprate.LimitAll(..).
+	//
+	// Please see _example/main.go for other more, or read the library code.
+	server.Router.Use(httprate.LimitByIP(100, 1*time.Minute))
+	server.Router.Use(middleware.StripSlashes)
+	server.Router.Use(middleware.RedirectSlashes)
+	server.Router.Use(middleware.Logger)
+	server.Router.Use(middleware.Recoverer)
+	server.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("healthchecked"))
+	})
+	server.Router.Post("/quote", handlers.CreateQuote(connectionPool))
+	server.Router.Get("/quote", handlers.ReadQuote(connectionPool))
+	server.Router.Patch("/quote", handlers.UpdateQuote(connectionPool))
+	server.Router.Delete("/quote", handlers.DeleteQuote(connectionPool))
+}
 
 func main() {
 
@@ -47,38 +93,10 @@ func main() {
 	// 	log.Fatal("Pinging database failed", err)
 	// }
 
-	chiRouter := chi.NewRouter()
-	chiRouter.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{`https:\/\/*`, `http:\/\/*`},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-	// Enable httprate request limiter of 100 requests per minute.
-	//
-	// In the code example below, rate-limiting is bound to the request IP address
-	// via the LimitByIP middleware handler.
-	//
-	// To have a single rate-limiter for all requests, use httprate.LimitAll(..).
-	//
-	// Please see _example/main.go for other more, or read the library code.
-	chiRouter.Use(httprate.LimitByIP(100, 1*time.Minute))
-	chiRouter.Use(middleware.RedirectSlashes)
-	chiRouter.Use(middleware.Logger)
-	chiRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("healthchecked"))
-	})
-	chiRouter.Post("/quote", handlers.CreateQuote(pool))
-	chiRouter.Get("/quote", handlers.ReadQuote(pool))
-	chiRouter.Patch("/quote", handlers.UpdateQuote(pool))
-	chiRouter.Delete("/quote", handlers.DeleteQuote(pool))
-	// log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), chiRouter))
+	server := createNewServer(os.Getenv("PORT"))
+	server.MountHandlers(pool)
 
-	server := &http.Server{Addr: "0.0.0.0:" + os.Getenv("PORT"), Handler: chiRouter}
+	serverHTTP := &http.Server{Addr: server.Addr, Handler: server.Router}
 
 	// Server run context
 	serverCtx, serverStopCtx := context.WithCancel(context.Background())
@@ -90,8 +108,8 @@ func main() {
 		<-sig
 
 		// Shutdown signal with grace period of 30 seconds
-		// lint:ignore
-		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+		shutdownCtx, cancelFn := context.WithTimeout(serverCtx, 30*time.Second)
+		defer cancelFn()
 
 		go func() {
 			<-shutdownCtx.Done()
@@ -101,7 +119,7 @@ func main() {
 		}()
 
 		// Trigger graceful shutdown
-		err := server.Shutdown(shutdownCtx)
+		err := serverHTTP.Shutdown(shutdownCtx)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -109,64 +127,11 @@ func main() {
 	}()
 
 	// Run the server
-	err = server.ListenAndServe()
+	err = serverHTTP.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
 
 	// Wait for server context to be stopped
 	<-serverCtx.Done()
-}
-
-// pgconfig is a struct that holds the configuration for connecting to a postgres database.
-// each field corresponds to a component of the connection string.
-// the following required environment variables are used to populate the struct:
-//
-//	PG_USER
-//	 PG_PASSWORD
-//	 PG_HOST
-//	 PG_PORT
-//	 PG_DATABASE
-//
-// additionally, the following optional environment variable is used to populate the sslmode:
-//
-//	PG_SSLMODE: must be one of  "", "disable", "allow", "require", "verify-ca", or "verify-full"
-type pgconfig struct {
-	user, database, host, password, port string // required
-	sslMode                              string // optional
-}
-
-func pgConfigFromEnv() (pgconfig, error) {
-	var missing []string
-	// small closures like this can help reduce code duplication and make intent clearer.
-	// you generally pay a small performance penalty for this, but for configuration, it's not a big deal;
-	// you can spare the nanoseconds.
-	// i prefer little helper functions like this to a complicated configuration framework like viper, cobra, envconfig, etc.
-	get := func(key string) string {
-		val := os.Getenv(key)
-		if val == "" {
-			missing = append(missing, key)
-		}
-		return val
-	}
-	cfg := pgconfig{
-		user:     get("PG_USER"),
-		database: get("PG_DATABASE"),
-		host:     get("PG_HOST"),
-		password: get("PG_PASSWORD"),
-		port:     get("PG_PORT"),
-		sslMode:  os.Getenv("PG_SSLMODE"), // optional, so we don't add it to missing
-	}
-	switch cfg.sslMode {
-	case "", "disable", "allow", "require", "verify-ca", "verify-full":
-		// valid sslmode
-	default:
-		return cfg, fmt.Errorf(`invalid sslmode "%s": expected one of "", "disable", "allow", "require", "verify-ca", or "verify-full"`, cfg.sslMode)
-	}
-
-	if len(missing) > 0 {
-		sort.Strings(missing) // sort for consistency in error message
-		return cfg, fmt.Errorf("missing required environment variables: %v", missing)
-	}
-	return cfg, nil
 }
